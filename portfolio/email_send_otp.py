aac492dc59691f9c7bbf97cf7601ff3ca0a4764b
Prem Sai Valluru <vallurupremsai590@gmail.com>
2026-05-03 20:39:19 +0530

    Change SMTP connection to use STARTTLS
    Updated email sending method to use STARTTLS for improved security.

--- portfolio/email_send_otp.py
+++ portfolio/email_send_otp.py
@@ -11,69 +11,9 @@ load_dotenv()
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 
-def send_otp_email(receiver_email):
-    otp = str(random.randint(100000, 999999))
-    subject = "Your OTP for Email Verification on Smart Time Table Management System"
-    body = f"Your OTP code is: {otp}"
-
-    msg = EmailMessage()
-    msg["From"] = SENDER_EMAIL
-    msg["To"] = receiver_email
-    msg["Subject"] = subject
-    msg.set_content(body)
-
-    try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
-            smtp.send_message(msg)
-        print("OTP Email sent successfully!")
-        return {"otp": otp, "status": "success", "message": "OTP sent successfully"}
-    except Exception as e:
-        print(f"Failed to send OTP email: {e}")
-        return {"status": "failure", "message": str(e)}
-    
-def send_swap_request(sender, sender_name, receiver_email, message):
-    subject = f"Swap Request from {sender_name}"
-    body = f"{message}\n\nNote: Login to the website to accept or reject the request!\n\nFrom Faculty: {sender}"
-    
-    msg = EmailMessage()
-    msg["From"] = SENDER_EMAIL
-    msg["To"] = receiver_email
-    msg["Subject"] = subject
-    msg.set_content(body)
-
-    try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
-            smtp.send_message(msg)
-        print("Swap Request Email sent successfully!")
-        return {"status": "success", "message": "Swap Request Email sent successfully"}
-    except Exception as e:
-        print(f"Failed to send Swap Request E#mail: {e}")
-        return {"status": "failure", "message": str(e)}
-
-def notify_student(receiver_email, message):
-    subject = f"Scheduled Lecture Swap!"
-    body = f"{message}"
-    
-    msg = EmailMessage()
-    msg["From"] = SENDER_EMAIL
-    msg["To"] = receiver_email
-    msg["Subject"] = subject
-    msg.set_content(body)
-
-    try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
-            smtp.send_message(msg)
-        print("Swap Request Email sent successfully!")
-        return {"status": "success", "message": "Swap Request Email sent successfully"}
-    except Exception as e:
-        print(f"Failed to send Swap Request E#mail: {e}")
-        return {"status": "failure", "message": str(e)}
-
 def send_email(subject, body):
     receiver_email = "vallurupremsai590@gmail.com"
+
     msg = EmailMessage()
     msg["From"] = SENDER_EMAIL
     msg["To"] = receiver_email
@@ -81,11 +21,16 @@ def send_email(subject, body):
     msg.set_content(body)
 
     try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
             smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
             smtp.send_message(msg)
+
         print("Email sent successfully!")
         return {"status": "success", "message": "Email sent successfully"}
+
     except Exception as e:
         print(f"Failed to send email: {e}")
         return {"status": "failure", "message": str(e)}
