666e4ca7d7e91ae809b88beff1e8abef3e937787
Prem Sai Valluru <vallurupremsai590@gmail.com>
2026-05-04 08:14:16 +0530

    Update settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
